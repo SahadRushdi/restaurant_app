24e21aa2fa63653e749325f5b073942e69ee5284
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-11-17 13:31:51 +0530

    Completed Cart page with all its function

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:restaurant_app/components/button.dart';
 import 'package:restaurant_app/models/shop.dart';
 import 'package:restaurant_app/theme/colors.dart';
 
@@ -8,32 +9,84 @@ import '../models/food.dart';
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
+  // remove from cart
+  void removeFromCart(Food food, BuildContext context) {
+  //   get access to the shop
+    final shop = context.read<Shop>();
+
+  //   remove form cart
+    shop.removeFromCart(food);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<Shop>(
       builder: (context, value, child) => Scaffold(
+        backgroundColor: primaryColor,
         appBar: AppBar(
           title: const Text("My Cart"),
+          elevation: 0,
           backgroundColor: primaryColor,
         ),
-        body:   ListView.builder(
-            itemCount: value.cart.length,
-            itemBuilder: (context, index) {
-              //   get food from cart
-              final Food food = value.cart[index];
+        body: Column(
+          children: [
+            // Customer Cart
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.cart.length,
+                itemBuilder: (context, index) {
+                  //   get food from cart
+                  final Food food = value.cart[index];
 
-              //   get food name
-              final String foodName = food.name;
+                  //   get food name
+                  final String foodName = food.name;
 
-              //   get food price
-              final String foodPrice = food.price;
+                  //   get food price
+                  final String foodPrice = food.price;
 
-              //   return list title
-              return ListTile(
-                title: Text(foodName),
-                subtitle: Text(foodPrice),
-              );
-            }
+                  //   return list title
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    margin: const EdgeInsets.only(left: 20, top: 20, right: 20),
+                    child: ListTile(
+                      title: Text(
+                        foodName,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        foodPrice,
+                        style: TextStyle(
+                          color: Colors.grey[200],
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.grey[300],
+                        ),
+                        onPressed: () => removeFromCart(food, context),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            //   Pay Button
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: MyButton(
+                text: "Pay Now",
+                onTap: () {},
+              ),
+            )
+          ],
         ),
       ),
     );
